0f4ee6ffae751e404676a2868395e3b206b1e9a8
Nick Harris <nharris85@gmail.com>
2016-02-26 16:07:28 +0000

    Fixed union and delete for useSingle mode

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,6 @@
 var Acl = require('../')
-  , tests = require('../node_modules/acl/test/tests')
-  , ArangoDBBackend = require('../');
+  , tests = require('../node_modules/acl/test/tests');
+
   describe('ArangoDB - Default', function () {
     before(function (done) {
       var self = this
@@ -21,25 +21,25 @@ var Acl = require('../')
   })
 
 
-  // describe('ArangoDB - useSingle', function () {
-  //   before(function (done) {
-  //     var self = this
-  //       , arangojs = require('arangojs')
-  //       , db = arangojs()
-  //
-  //       db.dropDatabase("test", function(err, data){
-  //           //ignore error and create test db
-  //           db.createDatabase('test', function(innererr, innerdata){
-  //               if(innererr) return done(innererr);
-  //               db.useDatabase('test');
-  //               self.backend = new Acl.arangodbBackend(db, "acl", true);
-  //               done();
-  //           });
-  //       })
-  //   })
-  //
-  //   run()
-  // })
+  describe('ArangoDB - useSingle', function () {
+    before(function (done) {
+      var self = this
+        , arangojs = require('arangojs')
+        , db = arangojs()
+
+        db.dropDatabase("test", function(err, data){
+            //ignore error and create test db
+            db.createDatabase('test', function(innererr, innerdata){
+                if(innererr) return done(innererr);
+                db.useDatabase('test');
+                self.backend = new Acl.arangodbBackend(db, "acl", true);
+                done();
+            });
+        })
+    })
+
+    run()
+  })
 
 function run() {
   Object.keys(tests).forEach(function (test) {
